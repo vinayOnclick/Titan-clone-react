4426c19dfd1e49490aeaf0422131ff8c993d0573
Rohit Sharma <rsharma9234@gmail.com>
2023-01-03 14:58:25 +0530

    server status

--- server/index.js
+++ server/index.js
@@ -4,15 +4,17 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 
 const app = express();
-const PORT = 4000;
 
 app.use(express.json());
 
-mongoose.connect("mongodb+srv://Shiwani:ZZNFkUnfQAYRhSz8@cluster0.4oxd0be.mongodb.net/test", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  "mongodb+srv://Shiwani:ZZNFkUnfQAYRhSz8@cluster0.4oxd0be.mongodb.net/test",
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  }
+);
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error: "));
@@ -20,7 +22,7 @@ db.once("open", function () {
   console.log("Connected successfully");
 });
 
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 4000;
 
 app.get("/api", (req, res) => res.sendStatus("Its Working"));
 
